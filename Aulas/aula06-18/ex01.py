eedf82d2629e55ad07eedad9cb8f2fab195a291f
Mori Hiroshi <mori.hiroshi.a.c@gmail.com>
2024-06-18 19:56:06 -0400

    teste

---
+++ Aulas/aula06-18/ex01.py
@@ -0,0 +1,24 @@
+expressao = 5.5
+
+def verifica_expressao(expressao):
+    if type(expressao) is not str:
+        return "Expressao tem que ser string"
+    lista = list(expressao)
+    pilha = []
+
+    for x in lista:
+        if x == "(":
+            pilha.append('(')
+        elif x == ")":
+            if not pilha:
+                return "incorreto"
+            pilha.pop()
+
+
+    if not pilha:
+        return "correto"
+    else:
+        return "nao correto"
+
+
+print(verifica_expressao(expressao))
